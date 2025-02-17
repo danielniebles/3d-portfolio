a97b50585855b7ff598261ceafb28c7ca382d703
danielniebles <danielf.niebles@gmail.com>
2025-02-17 14:17:33 -0500

    feat: add contact button

--- eslint.config.js
+++ eslint.config.js
@@ -35,6 +35,7 @@ export default [
       ],
 
       "react/no-unknown-property": ["off", { "ignore": ["JSX"] }],
+      "react/prop-types": ["off", { "ignore": ["JSX"] }]
     },
   },
 ];
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -10,6 +10,7 @@ import ReactLogo from "../components/ReactLogo";
 import Cube from "../components/Cube";
 import Rings from "../components/Rings";
 import HeroCamera from "../components/HeroCamera";
+import Button from "../components/Button";
 
 const Hero = () => {
   const isMobile = useMediaQuery({ maxWidth: 768 });
@@ -58,6 +59,11 @@ const Hero = () => {
             <directionalLight position={[10, 10, 10]} intensity={0.5} />
           </Suspense>
         </Canvas>
+      </div>
+      <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
+        <a href="#contact" className="w-fit">
+          <Button name="Let's work together" isBeam containerClass="sm:w-fit w-full sm:min-w-96"></Button>
+        </a>
       </div>
     </section>
   );
