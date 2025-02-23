5439497db110f76c6e3e7f01d1a829dcae3dcf37
danielniebles <danielf.niebles@gmail.com>
2025-02-23 16:14:14 -0500

    feat: add about section

---
+++ src/sections/About.jsx
@@ -0,0 +1,115 @@
+import Globe from "react-globe.gl";
+import Button from "../components/Button";
+import { useState } from "react";
+
+const About = () => {
+  const [copied, setCopied] = useState(false);
+  const handleCopy = () => {
+    navigator.clipboard.writeText("danielf.niebles@gmail.com");
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  };
+
+  return (
+    <section className="c-space my-20">
+      <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5">
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img
+              src="/assets/grid1.png"
+              alt="grid-1"
+              className="w-full sm:h-[276px] h-fit object-contain"
+            />
+            <div>
+              <p className="grid-headtext">Hi, I&apos;m Daniel</p>
+              <p className="grid-subtext">
+                With 5 years of experience honing my front end skills
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img
+              src="/assets/grid2.png"
+              alt="grid-2"
+              className="w-full sm:h-[276px] h-fit object-contain"
+            />
+            <div>
+              <p className="grid-headtext">Tech Stack</p>
+              <p className="grid-subtext">I specialize in ...</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-1 xl:row-span-4">
+          <div className="grid-container">
+            <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
+              <Globe
+                height={326}
+                width={326}
+                backgroundColor="rgba(0, 0, 0, 0)"
+                backgroundImageOpacity={0.5}
+                showAtmosphere
+                showGraticules
+                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+              />
+            </div>
+            <div>
+              <p className="grid-headtext">I work remotely</p>
+              <p className="grid-subtext">Based in...</p>
+              <Button
+                isBeam
+                name={"Contact Me"}
+                containerClass="w-full mt-10"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="xl:col-span-2 xl:row-span-3">
+          <div className="grid-container">
+            <img
+              src="/assets/grid3.png"
+              alt="grid-3"
+              className="w-full sm:h-[266px] h-first object-contain"
+            />
+            <div>
+              <p className="grid-headtext">I work remotely</p>
+              <p className="grid-subtext">Based in...</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="xl:col-span-1 xl:row-span-2">
+          <div className="grid-container">
+            <img
+              src="/assets/grid4.png"
+              alt="grid-4"
+              className="w-full md:h-[126px] sm:h-[276px] h-first object-cover sm:object-top "
+            />
+            <div className="space-y-2">
+              <p className="grid-subtext text-center">Contact me</p>
+              <div className="copy-container" onClick={handleCopy}>
+                <img
+                  src={`${copied ? "/assets/tick.svg" : "/assets/copy.svg"}`}
+                  alt="copy"
+                />
+                <p className="lg:text-2xl md:text-xl text-white text-gray_gradient font-medium">
+                  danielf.niebles@gmail.com
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
