4190ced2d58b3346c1db42162d9e02b8b6413595
danielniebles <danielf.niebles@gmail.com>
2025-02-23 16:36:47 -0500

    feat: add project description container

---
+++ src/sections/Projects.jsx
@@ -0,0 +1,53 @@
+import { myProjects } from "../../constants";
+
+const Projects = () => {
+  const currentProject = myProjects[0];
+  return (
+    <section className="c-space my-20">
+      <p className="head-text">My Selected Work</p>
+      <div className="grid lg:grid-cols-2 grid-cols-1 gap-5 w-full mt-12">
+        <div className="flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200">
+          <div className="absolute top-0 right-0">
+            <img
+              src={currentProject.spotlight}
+              alt="spotlight"
+              className="w-full h-96 object-cover rounded-xl"
+            />
+          </div>
+          <div
+            className="p-3 backdrop-filter backdrop-blur-3xl w-fit rounded-lg"
+            style={currentProject.logoStyle}
+          >
+            <img src={currentProject.logo} alt="logo" className="w-10 h-10" />
+          </div>
+          <div className="flex flex-col gap-5 text-white-600 my-5">
+            <p className="text-white text-2xl font-semibold animatedText">
+              {currentProject.title}
+            </p>
+            <p className="animatedText">{currentProject.desc}</p>
+          </div>
+          <div className="flex items-center justify-between flex-wrap gap-5">
+            <div className="flex items-center gap-3">
+              {currentProject.tags.map((tag, index) => (
+                <div key={index} className="tech-logo">
+                  <img src={tag.path} alt={tag.name} />
+                </div>
+              ))}
+            </div>
+            <a
+              href={currentProject.href}
+              target="_blank"
+              rel="noreferrer"
+              className="flex items-center gap-2 cursor-pointer text-white-600"
+            >
+              <p>Check Live Site</p>
+              <img src="/assets/arrow-up.png" alt="arrow" className="w-3 h-3" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
